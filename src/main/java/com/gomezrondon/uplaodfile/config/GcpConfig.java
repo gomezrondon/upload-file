ca4cd337bb9c9999befc3749f0663d8259945986
gomezrondon <gomezrondon@gmail.com>
2021-08-06 11:37:13 -0600

    Creating new Endpoint that uploads file and downloads an mp3 stt.

--- src/main/java/com/gomezrondon/uplaodfile/config/GcpConfig.java
+++ src/main/java/com/gomezrondon/uplaodfile/config/GcpConfig.java
@@ -5,11 +5,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -39,6 +41,14 @@ public class GcpConfig {
                 .getService();
     }
 
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+//                .setConnectTimeout(Duration.ofMillis(60000))
+//                .setReadTimeout(Duration.ofMillis(60000))
+                .build();
+    }
 
 
 }
--- src/main/java/com/gomezrondon/uplaodfile/controller/UploadFileController.java
+++ src/main/java/com/gomezrondon/uplaodfile/controller/UploadFileController.java
@@ -4,12 +4,19 @@ package com.gomezrondon.uplaodfile.controller;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
@@ -26,13 +33,15 @@ public class UploadFileController {
     private String bucketName;
     @Value("${gcs-file-object-name}")
     private String objectName;
-    @Value("${gcs-upload-flag:false}")
-    private Boolean isUpload;
+    @Value("${gcs-flask-app-url}")
+    private String flaskAppUrl;
 
     private final Storage storage;
+    private final RestTemplate restTemplate;
 
-    public UploadFileController(Storage storage) {
+    public UploadFileController(Storage storage, RestTemplate restTemplate) {
         this.storage = storage;
+        this.restTemplate = restTemplate;
         String currentDir = System.getProperty("user.dir");
         String separator = System.getProperty("file.separator");
         directory = currentDir + separator + "data" + separator;
@@ -52,21 +61,25 @@ public class UploadFileController {
         return "the time is: "+ LocalDateTime.now();
     }
 
-    @PostMapping("/file-upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    @PostMapping("/stt/lang/{language}")
+    public @ResponseBody byte[] uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String language) throws IOException {
         String filePath = directory + file.getOriginalFilename();
         file.transferTo(new File(filePath));
-        if (isUpload) {
-            this.upload(filePath);
-        }
-        return "success";
+        this.upload(filePath);
+        ResponseEntity<byte[]> responseEntity = getAudioFile(language);
+        System.out.println("Post request executed! ");
+        return responseEntity.getBody();
     }
 
-    @GetMapping(value = "/image") //just testing
-    public @ResponseBody byte[] getImage() throws IOException {
-        String file = directory + "thunbnail.png";
-        byte[] bytes = Files.readAllBytes(Paths.get(file));
-        return bytes;
+    private ResponseEntity<byte[]> getAudioFile(String language) {
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        var personJsonObject = new JSONObject();
+        personJsonObject.put("bucket", bucketName);
+        personJsonObject.put("lang", language);
+        HttpEntity<String> request = new HttpEntity<String>(personJsonObject.toString(), headers);
+        ResponseEntity<byte[]> responseEntity = restTemplate.postForEntity(flaskAppUrl+"/process", request, byte[].class);
+        return responseEntity;
     }
 
     public void upload(String filePath) throws IOException {
@@ -74,8 +87,7 @@ public class UploadFileController {
         BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
         storage.create(blobInfo, Files.readAllBytes(Paths.get(filePath)));
 
-        System.out.println(
-                "File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
+        System.out.println("File " + filePath + " uploaded to bucket " + bucketName + " as " + objectName);
     }
 
 
