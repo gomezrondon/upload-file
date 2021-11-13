3c493bcd3db11548138e5bab9866a1184465dc04
gomezrondon <gomezrondon@gmail.com>
2021-11-12 20:11:09 -0600

    medio funciona

---
+++ src/main/java/com/gomezrondon/uplaodfile/controller/PostBody.java
@@ -0,0 +1,30 @@
+package com.gomezrondon.uplaodfile.controller;
+
+public class PostBody {
+    private String bucket;
+    private String lang;
+
+    public PostBody() {
+    }
+
+    public PostBody(String bucket, String lang) {
+        this.bucket = bucket;
+        this.lang = lang;
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+}
--- src/main/java/com/gomezrondon/uplaodfile/controller/UploadFileController.java
+++ src/main/java/com/gomezrondon/uplaodfile/controller/UploadFileController.java
@@ -4,26 +4,30 @@ package com.gomezrondon.uplaodfile.controller;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @RestController
 public class UploadFileController {
@@ -37,11 +41,11 @@ public class UploadFileController {
     private String flaskAppUrl;
 
     private final Storage storage;
-    private final RestTemplate restTemplate;
+    private final WebClient.Builder builder;
 
-    public UploadFileController(Storage storage, RestTemplate restTemplate) {
+    public UploadFileController(Storage storage,  WebClient.Builder builder) {
         this.storage = storage;
-        this.restTemplate = restTemplate;
+        this.builder = builder;
         String currentDir = System.getProperty("user.dir");
         String separator = System.getProperty("file.separator");
         directory = currentDir + separator + "data" + separator;
@@ -62,24 +66,29 @@ public class UploadFileController {
     }
 
     @PostMapping("/stt/lang/{language}")
-    public @ResponseBody byte[] uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String language) throws IOException {
+    public @ResponseBody
+    byte[] uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String language) throws IOException {
         String filePath = directory + file.getOriginalFilename();
         file.transferTo(new File(filePath));
         this.upload(filePath);
-        ResponseEntity<byte[]> responseEntity = getAudioFile(language);
+        byte[] response = getAudioFile2(language);
         System.out.println("Post request executed! ");
-        return responseEntity.getBody();
+        return response;
     }
 
-    private ResponseEntity<byte[]> getAudioFile(String language) {
-        var headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        var personJsonObject = new JSONObject();
-        personJsonObject.put("bucket", bucketName);
-        personJsonObject.put("lang", language);
-        HttpEntity<String> request = new HttpEntity<String>(personJsonObject.toString(), headers);
-        ResponseEntity<byte[]> responseEntity = restTemplate.postForEntity(flaskAppUrl+"/process", request, byte[].class);
-        return responseEntity;
+    private byte[] getAudioFile2(String language) {
+        var postBody = new PostBody(bucketName, language);
+        Flux<DataBuffer> dataBufferFlux = builder.build()
+                .post()
+                .uri(flaskAppUrl + "/process")
+                .accept(MediaType.APPLICATION_OCTET_STREAM)
+                .body(Mono.just(postBody), PostBody.class)
+                .retrieve()
+                .bodyToFlux(DataBuffer.class);
+        String currentDir = System.getProperty("user.dir");
+        DataBufferUtils.write(dataBufferFlux, Path.of(currentDir+"/audio33.mp3"), StandardOpenOption.CREATE).block();
+
+        return null;
     }
 
     public void upload(String filePath) throws IOException {
