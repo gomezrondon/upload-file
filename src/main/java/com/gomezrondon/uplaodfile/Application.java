20df01d9faa43ce7115b93edc0fc01c2741b6d00
gomezrondon <gomezrondon@gmail.com>
2022-11-05 06:50:00 -0600

    Upgrading spring boot 3-rc1

--- src/main/java/com/gomezrondon/uplaodfile/Application.java
+++ src/main/java/com/gomezrondon/uplaodfile/Application.java
@@ -2,9 +2,7 @@ package com.gomezrondon.uplaodfile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.NativeHint;
 
-@NativeHint(options = "--enable-http --enable-https")
 @SpringBootApplication
 public class Application {
 
